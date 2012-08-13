2cd89fc8dfd863bcc9ba2193409835766c8ec7c3
Jacob Groundwater <groundwater@gmail.com>
2012-08-13 15:07:58 -0700

    Add API Implementation for User Data

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,31 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import com.dd.plist._
+
 object Application extends Controller {
   
+  def user(userid:String) = Action {
+    Logger.info("Get User Account: %s" format userid)
+    val nsdict = new NSDictionary()
+    
+    val byteArray = BinaryPropertyListWriter.writeToArray(nsdict)
+    Ok(byteArray).withHeaders("Content-Type"->"application/plist")
+  }
+  
+  def share(from:String,to:String) = Action {
+    Logger.info("Share from: %s to: %s" format (from,to))
+    TODO
+  }
+  
+  def register(device:String,user:String) = Action {
+    val deviceId = "None"
+    Logger.info("Register New Device: %s to User: %s" format (deviceId,user))
+    TODO
+  }
+  
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Redirect("http://cardtapapp.com")
   }
   
 }
---
+++ test/controllers/ApplicationTest.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+import com.dd.plist._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import com.dd.plist.NSDictionary
+
+@RunWith(classOf[JUnitRunner])
+class ApplicationSpec extends Specification {
+
+  "Users API" should {
+    "specity an application/plist type" in {
+      running(FakeApplication()) {
+        val result = controllers.Application.user("demo")(FakeRequest())
+        contentType(result) must beSome("application/plist")
+      }
+    }
+    "return an NSDictionary" in {
+      running(FakeApplication()) {
+        val result = controllers.Application.user("demo")(FakeRequest())
+        val nsobject = PropertyListParser.parse(contentAsBytes(result))
+        val nsdict = nsobject.asInstanceOf[NSDictionary]
+
+        nsdict must not be null
+      }
+    }
+  }
+}
