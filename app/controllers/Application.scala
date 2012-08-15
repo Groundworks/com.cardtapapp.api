e200afe5918b2c6175e769777df3f79a6138ef0d
Jacob Groundwater <groundwater@gmail.com>
2012-08-15 16:18:10 -0700

    Refactor to Extract User Data Actor

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,90 +1,114 @@
 package controllers
 
+import com.dd.plist._
+
 import play.api._
 import play.api.mvc._
 
-import com.dd.plist._
+import akka.actor._
+import akka.pattern._
+import akka.util._
 
+import play.api.Play.current
+import play.api.libs.concurrent.AkkaPromise
+import play.libs.Akka.system
 import java.util.UUID
 
 case class Registration(
-  email  : String,
-  device : String,
-  authcode : String,
-  var authorized : Boolean
-)
+  email: String,
+  device: String,
+  authcode: String,
+  var authorized: Boolean)
+
+case class UserData(email: String)
+class UserDataManager extends Actor {
+  def receive = {
+    case UserData(email) =>
+      val dict = new NSDictionary()
+      dict.put("Email", email)
+      sender ! dict
+
+  }
+}
 
 object Application extends Controller {
 
-  // Multiple Indexes for the Same Data //
-  val registration  = collection.mutable.Map[String,Registration]()
-  val authorization = collection.mutable.Map[String,Registration]()
+  implicit val timeout: Timeout = Timeout(Duration(5, "seconds"))
 
-  def sendEmailAuthorization(email:String,code:String){
+  val userDataManager = system.actorOf(Props[UserDataManager])
+
+  // Multiple Indexes for the Same Data //
+  val registration = collection.mutable.Map[String, Registration]()
+  val authorization = collection.mutable.Map[String, Registration]()
+
+  def sendEmailAuthorization(email: String, code: String) {
     Logger.info("Sending Authorization Code %s" format code)
   }
-  
+
   def register = Action { implicit request =>
     request.body.asFormUrlEncoded.map { form =>
       form.get("email").map { emails =>
-        
+
         val email = emails.head
         val uuid = UUID.randomUUID().toString()
         val auth = UUID.randomUUID().toString()
-        
-        val reg = Registration(email,uuid,auth,false)
-        
-        registration(uuid)  = reg
+
+        val reg = Registration(email, uuid, auth, false)
+
+        registration(uuid) = reg
         authorization(auth) = reg
-        
-        sendEmailAuthorization(email,auth)
-        
-        Logger.info("User %s Registered Device %s with Authorization Code %s" format (email,uuid,auth))
-        
-        Created.withHeaders("Location"-> routes.Application.login(uuid).toString )
-      }.getOrElse{BadRequest("Expecting Email in Form Data")}
-    }.getOrElse{
+
+        sendEmailAuthorization(email, auth)
+
+        Logger.info("User %s Registered Device %s with Authorization Code %s" format (email, uuid, auth))
+
+        Created.withHeaders("Location" -> routes.Application.login(uuid).toString)
+      }.getOrElse { BadRequest("Expecting Email in Form Data") }
+    }.getOrElse {
       BadRequest("Expecting Form Data")
     }
   }
   
-  def authorize(authcode:String) = Action { request =>
+  def authorize(authcode: String) = Action { request =>
     Logger.info("Authorizing with Code %s" format authcode)
-    authorization.get(authcode).map { registration => 
+    authorization.get(authcode).map { registration =>
       registration.authorized = true
       val path = routes.Application.login(registration.device).toString
       val prot = "cardtapapp+http"
-      val host = if(request.host==""){"localhost"}else{request.host}
-      val redirect = "%s://%s%s" format (prot,host,path)
-      Logger.debug("Redirecting to %s" format redirect )
-      Redirect( redirect )
-    }.getOrElse{NotFound}
+      val host = if (request.host == "") { "localhost" } else { request.host }
+      val redirect = "%s://%s%s" format (prot, host, path)
+      Logger.debug("Redirecting to %s" format redirect)
+      Redirect(redirect)
+    }.getOrElse { NotFound }
   }
   
-  def login(device:String) = Action{
+  def login(device: String) = Action {
     Logger.info("Attempting Device %s Log In" format device)
     registration.get(device).map { registration =>
-      if(registration.authorized){
+      if (registration.authorized) {
         Logger.info("Device %s Login Success" format device)
-        
-        // Construct User Data //
-        val dict = new NSDictionary()
-        dict.put("Email",registration.email)
-        val plistByteArray = BinaryPropertyListWriter.writeToArray(dict)
-        // ------------------- //
-        
-        Ok( plistByteArray ).withHeaders("Content-Type"->"application/plist")
-      }else{
+        val email = registration.email
+
+        // Send Async Request
+        Async {
+          new AkkaPromise(userDataManager ? UserData(email)) map {
+            case dict: NSDictionary =>
+              val plistByteArray = BinaryPropertyListWriter.writeToArray(dict)
+              Ok(plistByteArray).withHeaders("Content-Type" -> "application/plist")
+            case _ => InternalServerError
+          }
+        }
+      } else {
         Logger.info("Device %s Not Authorized" format device)
         Unauthorized("This Device Has Not Been Authorized")
       }
-    }.getOrElse{NotFound("")}
+    }.getOrElse { NotFound("") }
   }
 
   def dump = Action {
-    val regs = registration.foldLeft("\n")((out,next)=>out+next+"\n") + 
-               authorization.foldLeft("\n")((out,next)=>out+next+"\n")
+    val regs = registration.foldLeft("\n")((out, next) => out + next + "\n") +
+      authorization.foldLeft("\n")((out, next) => out + next + "\n")
     Ok(regs)
   }
-  
+
 }
