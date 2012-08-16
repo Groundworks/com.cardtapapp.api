c4dffbfb265919f12327b6b0f61fde1f2619e5f4
Jacob Groundwater <groundwater@gmail.com>
2012-08-15 20:37:56 -0700

    Add Card Database Schema

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,15 +2,26 @@
  
 # --- !Ups
 
-CREATE TABLE accounts (
+CREATE TABLE account (
     id SERIAL,
     userid     TEXT NOT NULL,
     authcode   TEXT NOT NULL,
     devicekey  TEXT NOT NULL,
-    authorized BOOLEAN NOT NULL,
+    authorized TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE card (
+    id SERIAL,
+    ownerPhone TEXT NOT NULL,
+    ownerName  TEXT NOT NULL,
+    ownerEmail TEXT NOT NULL,
+    imageFront TEXT NOT NULL,
+    imageRear  TEXT NOT NULL,
     PRIMARY KEY (id)
 );
  
 # --- !Downs
  
-DROP TABLE accounts;
+DROP TABLE account;
+DROP TABLE card;
