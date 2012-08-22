c469580a2ca054ead95ce5c29584cfcfbf058802
Jacob Groundwater <groundwater@gmail.com>
2012-08-21 22:40:52 -0700

    Format HMAC as Hex String

--- app/util/Utility.scala
+++ app/util/Utility.scala
@@ -1,6 +1,9 @@
 package util
 
 import java.util.{ UUID => JavaUUID }
+import play.api.Play
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 object Random {
   def uuid(): String = {
@@ -8,3 +11,18 @@ object Random {
   }
 }
 
+object HMac {
+  val secret = Play.current.configuration.getString("application.secret").get
+  
+  val mac = Mac.getInstance("HmacSHA256")
+  val key = new SecretKeySpec(secret.getBytes,"HmacSHA256")
+  
+  def sign(bytes:Array[Byte]): String = {
+    mac.init(key)
+    mac.doFinal(bytes).map{
+      b:Byte => "%02x" format b 
+    }.foldLeft("")((hex,str)=>str+hex)
+   
+  }
+  
+}
